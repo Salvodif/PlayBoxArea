8d5d3ce90f92d5ff4bd54e20fe1887df71178830
Salvodif <salvatore.difazio@gmail.com>
2024-04-30 20:50:56 +0200

    even a c# test

---
+++ CSharpPlaybox/FootnotesExtractor/Program.cs
@@ -0,0 +1,29 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace FootnotesExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"Salvatore Di Fazio.docx";
+            using (WordprocessingDocument myDocument = WordprocessingDocument.Open(fileName, true))
+            {
+                var footnotesPart = myDocument.MainDocumentPart.FootnotesPart;
+                if (footnotesPart != null)
+                {
+                    var footnotes = footnotesPart.Footnotes.Elements<Footnote>().Take(10);
+
+                    foreach (var footnote in footnotes)
+                    {
+                        Console.WriteLine(footnote.InnerText);
+                    }
+                }
+            }
+            Console.WriteLine("All done. Press a key.");
+            Console.ReadKey();
+        }
+    }
+}
--- PyPlaybox/footnotesextractor/footnotesextractor.py
+++ PyPlaybox/footnotesextractor/footnotesextractor.py
@@ -1,15 +1,52 @@
+import re
 from docx2python import docx2python
+from docx2python.iterators import iter_paragraphs
+
+def extract_footnotes(input_string):
+    pattern = r"footnote(\d+)\)\\t', '(.+?)'"
+
+    footnote_dict = {}
+
+    matches = re.findall(pattern, input_string)
+
+    for match in matches:
+        key = match[0]
+        value = match[1]
+
+        if "Ivi" in value or "Ibidem" in value:
+            continue
+
+        footnote_dict[key] = value
+
+    return footnote_dict
 
 
-biblio = {}
+def flatten_nested_list(nested_list):
+    flattened_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flattened_list.extend(flatten_nested_list(item))
+        else:
+            flattened_list.append(item)
+
+    return flattened_list
 
 
-class book:
-    def __init__(self, title: str, edition: str, year):
-        self.title = title
-        self.edition = edition
-        self.year = year
+with docx2python("Salvatore Di Fazio.docx") as output:
+    footnotes = output.footnotes
+    individual_footnote_strings = list(iter_paragraphs(footnotes))
+
+    footnotes_string = "".join(flatten_nested_list(footnotes))
+    print(individual_footnote_strings)
+
+#    lines = footnotes_string.splitlines()
+#    i = 1
+#    for line in lines:
+#        print(i)
+#        print(line + "\\n")
+#        i = i + 1
+
+    # for key, value in risultato.items():
+    #     print(f"Chiave: {key}\nValore: {value}\n")
 
 
-with docx2python("salvatore di fazio.docx") as docx_content:
-    print(docx_content.footnotes_runs)
